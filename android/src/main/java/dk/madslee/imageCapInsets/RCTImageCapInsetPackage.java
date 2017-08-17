ed5adbffcb4827ff328a3e8272c005bf8c3dadef
Kamal Mahyuddin <kamal@envoy.com>
2017-08-16 19:38:20 -0700

    Fix compilation error in RN 0.47.0

--- android/src/main/java/dk/madslee/imageCapInsets/RCTImageCapInsetPackage.java
+++ android/src/main/java/dk/madslee/imageCapInsets/RCTImageCapInsetPackage.java
@@ -16,7 +16,6 @@ public class RCTImageCapInsetPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
